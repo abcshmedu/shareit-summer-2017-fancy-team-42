32d7fc6d46c735c02282ba19680ccedbda9d04af
thomas murschall <thomas.murschall@hm.edu>
2017-04-24 15:42:07 +0200

    Deletedunused methods

--- src/main/java/edu/hm/shareit/Services/MediaServiceResult.java
+++ src/main/java/edu/hm/shareit/Services/MediaServiceResult.java
@@ -6,7 +6,7 @@ import javax.ws.rs.core.Response.Status;
  * @author Markus Krahl
  */
 public enum MediaServiceResult {
-    Succes(200, Status.ACCEPTED), Duplicate(400,Status.CONFLICT);
+    Succes(200, Status.ACCEPTED), Duplicate(400, Status.CONFLICT);
     private int code;
     private Status state;
 
@@ -33,21 +33,5 @@ public enum MediaServiceResult {
     Status getStatus() {
         return state;
     }
-
-    /*
-    MediaServiceResult valueOf(String ans){
-        switch (ans){
-            case "Succes":
-                return Succes;
-            case "BadParameter":
-                return BadParameter;
-            default:
-                return BadParameter;
-        }
-    }
-
-    MediaServiceResult[] values(){
-        return new MediaServiceResult[0];
-    }
-    */
+    
 }
